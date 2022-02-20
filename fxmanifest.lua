277d2bd092bf7f3c52a1bc6be3a611e43e5a5f32
Vyzzen <100099607+Vyzzen@users.noreply.github.com>
2022-02-21 00:20:56 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Manifest data
+fx_version 'cerulean'
+games {'gta5'}
+
+-- Resource stuff
+name 'vyz_DDoS'
+description 'Protection for DDoS Attacks'
+version 'v1.1'
+author 'Vyzzen'
+url 'https://discord.gg/z65sDx8X4x'
+
+
+files {
+    'vyz.cfg',
+}
